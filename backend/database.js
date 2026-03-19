d655292e1212778854d866616fccb57b01a44f7d
Santanu Saha <forever.santanu007@gmail.com>
2026-03-19 21:08:40 +0530

    Fix: switch to better-sqlite3

--- backend/database.js
+++ backend/database.js
@@ -1,15 +1,37 @@
-const sqlite3 = require("sqlite3").verbose();
+// const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./logs.db", (err) => {
-  if (err) {
-    console.error("Database error:", err);
-  } else {
-    console.log("Connected to SQLite database");
-  }
-});
+// const db = new sqlite3.Database("./logs.db", (err) => {
+//   if (err) {
+//     console.error("Database error:", err);
+//   } else {
+//     console.log("Connected to SQLite database");
+//   }
+// });
+
+// // Create table
+// db.run(`
+//   CREATE TABLE IF NOT EXISTS mail_logs (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     professor_name TEXT,
+//     university TEXT,
+//     email TEXT,
+//     attempts INTEGER,
+//     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+//     ip TEXT,
+//     location TEXT
+//   )
+// `);
+
+
+// module.exports = db;
+
+
+const Database = require("better-sqlite3");
+
+const db = new Database("logs.db");
 
 // Create table
-db.run(`
+db.prepare(`
   CREATE TABLE IF NOT EXISTS mail_logs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     professor_name TEXT,
@@ -20,6 +42,6 @@ db.run(`
     ip TEXT,
     location TEXT
   )
-`);
+`).run();
 
 module.exports = db;
